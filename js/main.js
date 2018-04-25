1be3fb1fa1bc9cb4ba721e2d56485dd6b50ddd39
Cristian Poley <cpoley@ubliz.com>
2018-04-25 19:37:14 +0200

    [feat] Transition added

--- js/main.js
+++ js/main.js
@@ -11,24 +11,23 @@ $.ajax({
     // código a ejecutar si la petición es satisfactoria;
     // la respuesta es pasada como argumento a la función
     success : function(json) {
-        var question = '';
-        var count    = 0;
-
         // Guardamos en el localStorage los datos de la consulta    
         localStorage.setItem("data", JSON.stringify(json.data));
-        // Recorremos cada pregunta
-        json.data.forEach(elem => {
-            count += 1;
-            question = '<div class="header-question">' + count + '/3' + '</div>';
-            question += '<div class="question"><h5 class="title-question">' + elem.question + '</h5>';
-            // Recorremos cada respuesta de la actual pregunta
-            question += '<div class="answers">';
-            elem.answers.forEach(answer => {
-                question += '<div class="answer"><img class="image-answer" src="' + answer.image + '"/>';
-                question += '<p>' + answer.text + '<p/></div>';
-            });
+        
+        // Puntuación del Quiz
+        localStorage.setItem('score', 0);
+
+        // Recorro todos los datos devueltos por el servicio y voy creando los divs necesarios
+        var question = '';
+        json.data.forEach((elem, index) => {
+            index += 1;   
+            question = '<div class="question" id="question' + index + '">';
+            question += '<div class="header-question">' + index + '/' + json.data.length + '</div>';
             question += '</div>';
-            $('.content').append(question + '</div>');            
+            // Guardamos en el localStorage cada pregunta y sus respuestas
+            localStorage.setItem("question" + index, JSON.stringify(json.data[index - 1]));
+            // Lo añadimos al HTML
+            $('.content').append(question);
         });
     },
  
@@ -45,11 +44,71 @@ $.ajax({
     }
 });
 
+/**
+ * Función invocada cuando se inicia el Quiz
+ */
 function initQuiz() {
-    $(".home").show().animate({ display: 'none !important' })
-    $(".content").show().animate({ display: 'initial' })
-    // console.log(JSON.parse(localStorage.getItem("data")));
+    $('.home').css('display', 'none' );
+    var question1 = createQuestion(JSON.parse(localStorage.getItem('question1')), 1);
+    $('#question1').append(question1);  
+    $('#question1').css('display', 'inherit');
 }
 
+/**
+ * Método invocado al clicar en una respuesta de las posibles respuestas de una pregunta
+ */
+function next(elem) {
+    // Actualizamos la puntuación total del Quiz
+    localStorage.setItem('score', 2 + parseInt(localStorage.getItem('score')));
+    
+    // Si es la última pregunta invocamos a finishQuiz()
+    if (elem == JSON.parse(localStorage.getItem('data')).length) {
+        $('#question' + elem).css('display', 'none');
+        this.finishQuiz();
+    } else {
+        // Rellenamos el nuevo div
+        var newQuestion = createQuestion(JSON.parse(localStorage.getItem('question' + parseInt(elem + 1))), elem + 1);
+        $('#question' + parseInt(elem + 1)).append(newQuestion);
+
+        // Pasamos a la siguiente pregunta
+        $('#question' + elem).css('display', 'none');
+        $('#question' + parseInt(elem + 1)).css('display', 'inherit');
+    }
+}
+
+/**
+ * Método invocado al clicar en la "X" de las cabeceras de las preguntas, al lado
+ * del número de la pregunta actual. Cancela el Quiz y vuelve a la pantalla principal.
+ */
+function cancelQuiz() {
+    // Ocultamos todos el contenido y restauramos el valor del score
+    $('.home').css('display', 'inherit');
+    $('.content').css('display', 'none');
+    localStorage.setItem('score', 0);
+}
+
+/**
+ * Método para finalizar el Quiz, mostrando la pantalla de resultado.
+ */
 function finishQuiz() {
+    $('.results').show();
+    $('#score').html(localStorage.getItem('score'));
+}
+
+/**
+ * Función para devolver el HTML montado según los datos que reciba por parámetro
+ * @param {*} data : Los datos con los que se va a montar el HTML 
+ */
+function createQuestion(data, currentQuestion) {
+    var html = 
+        '<h5 class="title-question">' + data.question + '</h5>' +
+        '<div class="answers">';
+        data.answers.forEach((elem, index) => {
+            html += '<div class="answer" onclick="next(' + parseInt(currentQuestion) + ')"><img class="image-answer" src="' + elem.image + '">';
+            html += '<p>' + elem.text + '</p>';
+            html += '</div>';
+        });
+    html += '</div>';
+
+    return html;
 }
