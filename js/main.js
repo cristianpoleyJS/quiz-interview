aa6e7bfcc19fdd1fe2e0fd3196c712b473317360
Cristian Poley <cpoley@ubliz.com>
2018-04-25 19:54:16 +0200

    [feat] Added view "Results"

--- js/main.js
+++ js/main.js
@@ -1,48 +1,53 @@
-$.ajax({
-    // la URL para la petición
-    url : 'http://dws.aonate.lab.cloudioo.net/prueba-front/get',
- 
-    // especifica si será una petición POST o GET
-    type : 'GET',
+/**
+ * Invoca al servicio
+ */
+function getData() {
+    $.ajax({
+        // la URL para la petición
+        url : 'http://dws.aonate.lab.cloudioo.net/prueba-front/get',
     
-    // Se espera como tipo de dato devuelto por el servicio un 'jsonp'
-    dataType: "jsonp",
-
-    // código a ejecutar si la petición es satisfactoria;
-    // la respuesta es pasada como argumento a la función
-    success : function(json) {
-        // Guardamos en el localStorage los datos de la consulta    
-        localStorage.setItem("data", JSON.stringify(json.data));
+        // especifica si será una petición POST o GET
+        type : 'GET',
         
-        // Puntuación del Quiz
-        localStorage.setItem('score', 0);
+        // Se espera como tipo de dato devuelto por el servicio un 'jsonp'
+        dataType: "jsonp",
 
-        // Recorro todos los datos devueltos por el servicio y voy creando los divs necesarios
-        var question = '';
-        json.data.forEach((elem, index) => {
-            index += 1;   
-            question = '<div class="question" id="question' + index + '">';
-            question += '<div class="header-question">' + index + '/' + json.data.length + '</div>';
-            question += '</div>';
-            // Guardamos en el localStorage cada pregunta y sus respuestas
-            localStorage.setItem("question" + index, JSON.stringify(json.data[index - 1]));
-            // Lo añadimos al HTML
-            $('.content').append(question);
-        });
-    },
- 
-    // código a ejecutar si la petición falla;
-    // son pasados como argumentos a la función
-    // el objeto de la petición en crudo y código de estatus de la petición
-    error : function(xhr, status) {
-        alert('Disculpe, existió un problema');
-    },
- 
-    // código a ejecutar sin importar si la petición falló o no
-    complete : function(xhr, status) {
-        
-    }
-});
+        // código a ejecutar si la petición es satisfactoria;
+        // la respuesta es pasada como argumento a la función
+        success : function(json) {
+            // Guardamos en el localStorage los datos de la consulta    
+            localStorage.setItem("data", JSON.stringify(json.data));
+            
+            // Puntuación del Quiz
+            localStorage.setItem('score', 0);
+
+            // Recorro todos los datos devueltos por el servicio y voy creando los divs necesarios
+            var question = '';
+            json.data.forEach((elem, index) => {
+                index += 1;   
+                question = '<div class="question" id="question' + index + '">';
+                question += '<div class="header-question">' + index + '/' + json.data.length + '</div>';
+                question += '</div>';
+                // Guardamos en el localStorage cada pregunta y sus respuestas
+                localStorage.setItem("question" + index, JSON.stringify(json.data[index - 1]));
+                // Lo añadimos al HTML
+                $('.content').append(question);
+            });
+        },
+    
+        // código a ejecutar si la petición falla;
+        // son pasados como argumentos a la función
+        // el objeto de la petición en crudo y código de estatus de la petición
+        error : function(xhr, status) {
+            alert('Disculpe, existió un problema');
+        },
+    
+        // código a ejecutar sin importar si la petición falló o no
+        complete : function(xhr, status) {
+            
+        }
+    });
+}
 
 /**
  * Función invocada cuando se inicia el Quiz
@@ -83,8 +88,10 @@ function next(elem) {
 function cancelQuiz() {
     // Ocultamos todos el contenido y restauramos el valor del score
     $('.home').css('display', 'inherit');
-    $('.content').css('display', 'none');
+    $('.content').children().remove();
+    $('.results').css('display', 'none');
     localStorage.setItem('score', 0);
+    this.getData();
 }
 
 /**
@@ -112,3 +119,6 @@ function createQuestion(data, currentQuestion) {
 
     return html;
 }
+
+// Se invoca al servicio cada vez que carga el fichero, es decir, cada vez que carga la página
+this.getData();
